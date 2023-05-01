299df610e31007eaaf224f25b6beec85b434bf83
rathourdeepak07 <132257584+rathourdeepak07@users.noreply.github.com>
2023-05-01 14:38:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit
 
 streamlit.title('My first time in Python🥣 🥗 🐔 🥑🍞')
-
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
