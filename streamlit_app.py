9bc33b9c77988885823156e8fd9e85fc4fd11f9a
rathourdeepak07 <132257584+rathourdeepak07@users.noreply.github.com>
2023-05-01 14:21:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ import streamlit
 streamlit.title('My first time in Python')
 
 streamlit.title('My second time in Python')
+
+streamlit.header(' this is not a header')
+
+Streamlit.test(' "let's" play arround')
