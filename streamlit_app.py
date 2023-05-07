69047a14b26a593542a0fbc3e67f3d454dd0fc59
rathourdeepak07 <132257584+rathourdeepak07@users.noreply.github.com>
2023-05-07 15:22:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,3 +57,7 @@ streamlit.header("New Fruit box")
 fruit_choice1 = streamlit.text_input('What fruit would you like information about?','Banana')
 streamlit.write('The user entered ', fruit_choice1)
 
+my_cur.execute("insert into fruit_load_list values('from Streamlit')")
+
+
+
